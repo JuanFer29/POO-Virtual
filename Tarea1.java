911d84837e9e0169cffe59db467c9903488297e1
JuanFer29 <150398676+JuanFer29@users.noreply.github.com>
2023-12-13 20:32:00 -0500

    Add files via upload

---
+++ Tarea1.java
@@ -0,0 +1,15 @@
+
+package Tarea1;
+
+
+import Tarea1.Persona;
+
+public class Tarea1 {
+
+    public static void main(String[] args) {
+       
+        Persona personal = new Persona("Juan", "Quinteros", 21);
+        personal.setId(1750253617);   
+        System.out.println(personal.getId());
+    }
+}
