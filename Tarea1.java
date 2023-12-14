11551c10bfe0d0aa60659e81a4721aa6f54cb839
JuanFer29 <150398676+JuanFer29@users.noreply.github.com>
2023-12-13 21:02:18 -0500

    Update Tarea1.java

--- Tarea1.java
+++ Tarea1.java
@@ -9,7 +9,7 @@ public class Tarea1 {
     public static void main(String[] args) {
        
         Persona personal = new Persona("Juan", "Quinteros", 21);
-        personal.setId(1750253617);   
+        personal.setId(1750789617);   
         System.out.println(personal.getId());
     }
 }
